cf2fc3a4343b7e5bd0e26f0b93c9e528f80ad636
Jemila Bekele <jemilabekele17@gmail.com>
2026-04-16 17:02:42 +0300

    your message

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
-import authConfig from '@/auth.config';
-import NextAuth from 'next-auth/next';
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+ 
+//change
 
 const handler = NextAuth(authConfig);
+
 export { handler as GET, handler as POST };
